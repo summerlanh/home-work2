0fa417f19e9cd57bf3cebeedcc37ebea40a6a55c
summerlanh <summerlanh@hotmail.com>
2017-04-08 20:47:21 -0700

    Apr.8

---
+++ FullMVCSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FullMVCSite.MvcApplication" Language="C#" %>
---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(Models.GuestResponse guestResponse)
+        {
+            return View("Thanks", guestResponse);
+        }
+    }
+}
